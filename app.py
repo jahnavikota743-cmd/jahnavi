d4dbc94ed732d921c0000ef9d113353c44ae850a
jahnavikota743-cmd <jahnavikota743@gmail.com>
2026-06-12 06:18:50 +0000

    hi

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ name = st.text_input("Enter your name")
 if name: st.success(f"Hello {name}")
 age = st.slider("Select your age", 1, 100)
 st.write("Age:", age)
-if st.button("Celebrate"): st.balloons()
+if st.button("Celebrate"): 
+    st.balloons()
